7ce2ac284da9383f26269da3ff56bbc59c715665
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2020-10-26 10:33:55 -0700

    Initial commit of demo

---
+++ demo/discrete_integral_transform.py
@@ -0,0 +1,115 @@
+import numpy as np
+
+## Prepare width-axes based on number of gridpoints and linewidth data:
+def init_w_axis(dx, log_wi):
+    log_w_min = np.min(log_wi)
+    log_w_max = np.max(log_wi) + 1e-4
+    N = np.ceil((log_w_max - log_w_min)/dx) + 1
+    log_w_arr = log_w_min + dx * np.arange(N)
+    return log_w_arr
+
+
+## Calculate grid indices and grid alignments:   
+def get_indices(arr_i, axis):
+    pos    = np.interp(arr_i, axis, np.arange(axis.size))
+    index  = pos.astype(int) 
+    return index, index + 1, pos - index 
+
+
+## Calculate lineshape distribution matrix:
+def calc_matrix(v, log_wG, log_wL, v0i, log_wGi, log_wLi, S0i, aw_kind):
+    
+    #  Initialize matrix:
+    S_klm = np.zeros((2 * v.size, log_wG.size, log_wL.size))
+
+    #  Calculate grid indices and alignment:    
+    ki0, ki1, tvi = get_indices(v0i, v)
+    li0, li1, tGi = get_indices(log_wGi, log_wG)
+    mi0, mi1, tLi = get_indices(log_wLi, log_wL)
+
+    # Calculate weights:
+    if aw_kind == "optimized":
+        
+        dxG = (log_wG[-1] - log_wG[0])/(log_wG.size - 1)
+        dxL = (log_wL[-1] - log_wL[0])/(log_wL.size - 1)
+        dv     = (v[-1] - v[0])/(v.size - 1)
+        
+        C1_GG = ((6*np.pi-16)/(15*np.pi-32))**(2/3)
+        C1_LG = ((6*np.pi-16)/(3*(2*np.pi)**0.5))**(4/9)
+        C2_GG = (2/15)**(2/3)
+        C2_LG = (4/15)**(4/9)
+
+        alpha_i = np.exp(log_wLi - log_wGi)*(np.log(2))**0.5
+        wGi = np.exp(log_wGi)
+
+        R_GGi = 1/(C1_GG + C2_GG*alpha_i**(4/3))**(3/2) - 2
+        R_GLi = 2*alpha_i**2
+        R_Gvi = -8*np.log(2)
+        
+        R_LLi = -1
+        R_LGi = -1/(C1_LG*alpha_i**(4/9) + C2_LG*alpha_i**(16/9))**(9/4)
+
+        avi = tvi
+
+        aGi = tGi + (R_GGi*tGi*(1-tGi)*dxG**2 +
+                     R_GLi*tLi*(1-tLi)*dxL**2 +
+                     R_Gvi*tvi*(1-tvi)*(dv/wGi)**2)/(2*dxG)
+        
+        aLi = tLi + (R_LLi*tLi*(1-tLi)*dxL**2 +
+                     R_LGi*tGi*(1-tGi)*dxG**2)/(2*dxL)
+
+
+    else:
+        assert aw_kind == "linear"
+        avi = tvi
+        aGi = tGi
+        aLi = tLi
+    
+    # Add lines to spectral matrix:
+    np.add.at(S_klm, (ki0, li0, mi0), S0i * (1-avi) * (1-aGi) * (1-aLi))
+    np.add.at(S_klm, (ki0, li0, mi1), S0i * (1-avi) * (1-aGi) *    aLi )
+    np.add.at(S_klm, (ki0, li1, mi0), S0i * (1-avi) *    aGi  * (1-aLi))
+    np.add.at(S_klm, (ki0, li1, mi1), S0i * (1-avi) *    aGi  *    aLi )
+    np.add.at(S_klm, (ki1, li0, mi0), S0i *    avi  * (1-aGi) * (1-aLi))
+    np.add.at(S_klm, (ki1, li0, mi1), S0i *    avi  * (1-aGi) *    aLi )
+    np.add.at(S_klm, (ki1, li1, mi0), S0i *    avi  *    aGi  * (1-aLi))
+    np.add.at(S_klm, (ki1, li1, mi1), S0i *    avi  *    aGi  *    aLi )
+    
+    return S_klm
+
+
+## Apply transform:
+def apply_transform(v,log_wG,log_wL,S_klm):
+
+    dv    = (v[-1] - v[0]) / (v.size - 1)
+    x     = np.arange(v.size + 1) / (2 * v.size * dv)
+
+    # Sum over spectral matrix:
+    S_k_FT = np.zeros(v.size + 1, dtype = np.complex64)
+    for l in range(log_wG.size):
+        for m in range(log_wL.size):
+            wG,wL = np.exp(log_wG[l]),np.exp(log_wL[m])
+            gG_FT = np.exp(-(np.pi*x*wG)**2/(4*np.log(2)))
+            gL_FT = np.exp(-np.pi*x*wL)
+            gV_FT = gG_FT * gL_FT
+            S_k_FT += np.fft.rfft(S_klm[:,l,m]) * gV_FT
+    
+    return np.fft.irfft(S_k_FT)[:v.size] / dv
+
+
+## Synthesize spectrum:
+def synthesize_spectrum(v, dxG, dxL, v0i, log_wGi, log_wLi, S0i, aw_kind = 'linear'):
+
+    # Only process lines within range:
+    idx = (v0i >= np.min(v)) & (v0i < np.max(v))
+    v0i, log_wGi, log_wLi, S0i = v0i[idx], log_wGi[idx], log_wLi[idx], S0i[idx]
+
+    # Initialize width-axes:
+    log_wG = init_w_axis(dxG,log_wGi)
+    log_wL = init_w_axis(dxL,log_wLi)
+
+    # Calculate spectral matrix & apply transform:
+    S_klm = calc_matrix(v, log_wG, log_wL, v0i, log_wGi, log_wLi, S0i, aw_kind)
+    I = apply_transform(v, log_wG, log_wL, S_klm)
+        
+    return I,S_klm
---
+++ demo/show_spectrum.py
@@ -0,0 +1,36 @@
+from HITEMP_spectra import init_database, calc_stick_spectrum
+from discrete_integral_transform import synthesize_spectrum
+import numpy as np
+import matplotlib.pyplot as plt
+from time import perf_counter
+
+
+## Download database files from https://hitran.org/hitemp/
+
+HITEMP_path = "C:/HITEMP/"
+init_database([HITEMP_path + "02_2000-2125_HITEMP2010.par",
+               HITEMP_path + "02_2125-2250_HITEMP2010.par",
+               HITEMP_path + "02_2250-2500_HITEMP2010.par"])
+
+v_min = 2200.0 #cm-1
+v_max = 2400.0 #cm-1
+dv =     0.001 #cm-1
+v = np.arange(v_min,v_max,dv) #cm-1
+
+dxG = 0.2
+dxL = 0.2
+
+T = 1000.0 #K
+p =    0.1 #bar
+
+t0 = perf_counter()
+v0i,log_wGi,log_wLi,S0i = calc_stick_spectrum(p,T)
+I0,S_klm = synthesize_spectrum(v,dxG,dxL,v0i,log_wGi,log_wLi,S0i,aw_kind = 'optimized')
+print(perf_counter()-t0,'s')
+
+plt.plot(v,I0,'-')
+
+plt.xlim(v_max,v_min)
+plt.grid(True,alpha=0.2)
+plt.axhline(0,c='k',lw=1)
+plt.show()
