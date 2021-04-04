2f9c9203ddda0b54574004b0e2e30eadfa8ac5a1
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2021-04-04 12:47:13 -0700

    Added calculation of jacobian

--- demo/discrete_integral_transform.py
+++ demo/discrete_integral_transform.py
@@ -52,7 +52,7 @@ def calc_matrix(v, log_wG, log_wL, v0i, log_wGi, log_wLi, S0i, optimized):
         R_LL = 1
         R_LG = 1 / (C1_LG * alpha_i ** (1 / 2.25) + C2_LG * alpha_i ** (4 / 2.25)) ** 2.25
         
-        avi = tvi
+        avi = tvi #+ 7.12441305950051E-09 #TO-DO: What's this doing here?
 
         aGi = tGi + (R_Gv * tvi * (tvi - 1) * dxvGi**2 +
                      R_GG * tGi * (tGi - 1) * dxG**2 +
@@ -66,6 +66,26 @@ def calc_matrix(v, log_wG, log_wL, v0i, log_wGi, log_wLi, S0i, optimized):
         avi = tvi
         aGi = tGi
         aLi = tLi
+
+    indices = [ (ki0, li0, mi0),
+                (ki0, li0, mi1),
+                (ki0, li1, mi0),
+                (ki0, li1, mi1),
+                (ki1, li0, mi0),
+                (ki1, li0, mi1),
+                (ki1, li1, mi0),
+                (ki1, li1, mi1)]
+
+    weights = [S0i * (1-avi) * (1-aGi) * (1-aLi)),
+               S0i * (1-avi) * (1-aGi) *    aLi ),
+               S0i * (1-avi) *    aGi  * (1-aLi)),
+               S0i * (1-avi) *    aGi  *    aLi ),
+               S0i *    avi  * (1-aGi) * (1-aLi)),
+               S0i *    avi  * (1-aGi) *    aLi ),
+               S0i *    avi  *    aGi  * (1-aLi)),
+               S0i *    avi  *    aGi  *    aLi )]
+
+                  
     
     # Add lines to matrix -- Eqs 2.8 & 2.10:
     np.add.at(S_klm, (ki0, li0, mi0), S0i * (1-avi) * (1-aGi) * (1-aLi))
@@ -90,7 +110,7 @@ def calc_gV_FT(x,wG,wL):
 def apply_transform(v,log_wG,log_wL,S_klm,folding_thresh):
 
     dv     = (v[-1] - v[0]) / (v.size - 1)
-    x      = np.arange(v.size + 1) / (2 * v.size * dv)
+    x      = np.fft.rfftfreq(2 * v.size, dv)
     x_fold = (x,x[::-1])
 
     # Sum over lineshape distribution matrix -- Eqs 3.2 & 3.3:
@@ -108,7 +128,7 @@ def apply_transform(v,log_wG,log_wL,S_klm,folding_thresh):
                     
             S_k_FT += np.fft.rfft(S_klm[:,l,m]) * gV_FT
     
-    return np.fft.irfft(S_k_FT)[:v.size] / dv
+    return np.fft.irfft(S_k_FT)[:v.size] / dv, S_k_FT
 
 
 ## Synthesize spectrum:
@@ -123,7 +143,124 @@ def synthesize_spectrum(v, v0i, log_wGi, log_wLi, S0i, dxG = 0.14, dxL = 0.2, op
     log_wL = init_w_axis(dxL,log_wLi) #Eq 3.9
 
     # Calculate matrix & apply transform:
-    S_klm = calc_matrix(v, log_wG, log_wL, v0i, log_wGi, log_wLi, S0i, optimized)
-    I = apply_transform(v, log_wG, log_wL, S_klm, folding_thresh)                               
+    S_klm, weigths, indices = calc_matrix(v, log_wG, log_wL, v0i, log_wGi, log_wLi, S0i, optimized)
+    I,I_FT = apply_transform(v, log_wG, log_wL, S_klm, folding_thresh)                               
         
     return I,S_klm
+
+
+def calc_jacobian(v, v0i, log_wGi, log_wLi, S0i, Isyn, Iexp, dxG = 0.14, dxL = 0.2, optimized = False, folding_thresh = 1e-6):
+
+    # Only process lines within range:
+    idx = (v0i >= np.min(v)) & (v0i < np.max(v))
+    v0i, log_wGi, log_wLi, S0i = v0i[idx], log_wGi[idx], log_wLi[idx], S0i[idx]
+
+    # Initialize width-axes:
+    log_wG = init_w_axis(dxG,log_wGi) #Eq 3.8
+    log_wL = init_w_axis(dxL,log_wLi) #Eq 3.9
+
+    #"Reverse" of apply_transform():
+    dv  = (v[-1] - v[0]) / (v.size - 1)
+    x = np.fft.rfftfreq(2 * v.size, dv)
+
+    DeltaI = np.zeros(2*v.size)
+    DeltaI[:v.size] = Iexp - Isyn
+    DeltaI_FT = np.fft.rfft(DeltaI) * dv
+
+    Ix  = np.zeros((2 * v.size, log_wG.size, log_wL.size + 1))
+    IS0 = np.zeros((2 * v.size, log_wG.size, log_wL.size))
+    Iv0 = np.zeros((2 * v.size, log_wG.size, log_wL.size))
+    IwG = np.zeros((2 * v.size, log_wG.size, log_wL.size))
+    IwL = np.zeros((2 * v.size, log_wG.size, log_wL.size))
+
+    for l in range(log_wG.size):
+        for m in range(log_wL.size + 1):
+            wG_l,wL_m = np.exp(log_wG[l]),np.exp(log_wL[m] - 0.5 * dxL)
+            gV_FT = calc_gV_FT(x,wG_l,wL_m)
+
+            # Apply folding if needed: 
+            n = 1
+            while calc_gV_FT(n/(2*dv),wG_l,wL_m) >= folding_thresh:
+                gV_FT += calc_gV_FT(n/(2*dv) + x_fold[n&1],wG_l,wL_m)
+                n += 1
+
+        Ix[:,l,m] = np.fft.irfft(DeltaI_FT * gV_FT)
+
+        if m:
+            IS0[ :  ,l,m-1] = (Ix[:,l,m-1] + x[:,l,m]) / 2
+            
+            Iv0[1:-1,l,m-1] = (IS0[2:,l,m-1] - IS0[:-2,l,m-1]) / (2*dv)
+            Iv0[0   ,l,m-1] = (IS0[1 ,l,m-1] - IS0[ -1,l,m-1]) / (2*dv)
+
+            IwG[1:-1,l,m-1] = (IS0[2:,l,m-1] + IS0[:-2,l,m-1] - 2 * IS0[1:-1,l,m-1]) / (8 * np.log(2) * dv**2)
+            IwG[0   ,l,m-1] = (IS0[1 ,l,m-1] + IS0[ -1,l,m-1] - 2 * IS0[0   ,l,m-1]) / (8 * np.log(2) * dv**2)
+
+            wL_m *= np.exp(0.5 * dxL)
+            IwL[ :  ,l,m-1] = (Ix[:,l,m] - Ix[:,l,m-1]) / (wL_m * dxL)
+            
+
+    #"Reverse" of calc_matrix():   
+    ki0, ki1, tvi = get_indices(v0i, v)          #Eqs 3.4 & 3.6
+    li0, li1, tGi = get_indices(log_wGi, log_wG) #Eqs 3.7 & 3.10
+    mi0, mi1, tLi = get_indices(log_wLi, log_wL) #Eqs 3.7 & 3.10
+
+    # Calculate weights: TO-DO: check if optimized weigths work as well (don't see why not)
+    avi = tvi
+    aGi = tGi
+    aLi = tLi
+
+    a000 = (1-avi) * (1-aGi) * (1-aLi))
+    a001 = (1-avi) * (1-aGi) *    aLi )
+    a010 = (1-avi) *    aGi  * (1-aLi))
+    a011 = (1-avi) *    aGi  *    aLi )
+    a100 =    avi  * (1-aGi) * (1-aLi))
+    a101 =    avi  * (1-aGi) *    aLi )
+    a110 =    avi  *    aGi  * (1-aLi))
+    a111 =    avi  *    aGi  *    aLi )
+
+    int_DS0 = (a000 * IS0[(ki0,li0,mi0)] +
+               a001 * IS0[(ki0,li0,mi1)] +
+               a010 * IS0[(ki0,li1,mi0)] +
+               a011 * IS0[(ki0,li1,mi1)] +
+               a100 * IS0[(ki1,li0,mi0)] +
+               a101 * IS0[(ki1,li0,mi1)] +
+               a110 * IS0[(ki1,li1,mi0)] +
+               a111 * IS0[(ki1,li1,mi1)])
+
+    int_Dv0 = (a000 * Iv0[(ki0,li0,mi0)] +
+               a001 * Iv0[(ki0,li0,mi1)] +
+               a010 * Iv0[(ki0,li1,mi0)] +
+               a011 * Iv0[(ki0,li1,mi1)] +
+               a100 * Iv0[(ki1,li0,mi0)] +
+               a101 * Iv0[(ki1,li0,mi1)] +
+               a110 * Iv0[(ki1,li1,mi0)] +
+               a111 * Iv0[(ki1,li1,mi1)]) * Si0
+
+    int_DwG = (a000 * IwG[(ki0,li0,mi0)] +
+               a001 * IwG[(ki0,li0,mi1)] +
+               a010 * IwG[(ki0,li1,mi0)] +
+               a011 * IwG[(ki0,li1,mi1)] +
+               a100 * IwG[(ki1,li0,mi0)] +
+               a101 * IwG[(ki1,li0,mi1)] +
+               a110 * IwG[(ki1,li1,mi0)] +
+               a111 * IwG[(ki1,li1,mi1)]) * Si0 * np.exp(log_wGi)
+    
+    int_DwL = (a000 * IwL[(ki0,li0,mi0)] +
+               a001 * IwL[(ki0,li0,mi1)] +
+               a010 * IwL[(ki0,li1,mi0)] +
+               a011 * IwL[(ki0,li1,mi1)] +
+               a100 * IwL[(ki1,li0,mi0)] +
+               a101 * IwL[(ki1,li0,mi1)] +
+               a110 * IwL[(ki1,li1,mi0)] +
+               a111 * IwL[(ki1,li1,mi1)]) * Si0
+
+    int1 = np.sum(DeltaI*Iexp)*dv
+    int2 = np.sum(DeltaI**2)*dv
+    
+    dLdS0 = 2 * (int1 - int_DS0) / int2
+    dLdv0 = 2 * (int1 - int_Dv0) / int2
+    dLdwG = 2 * (int1 - int_DwG) / int2
+    dLdwL = 2 * (int1 - int_DwL) / int2
+    
+    return dLdS0, dLdv0, dLdwG, dLdwL
+    
