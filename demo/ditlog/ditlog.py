5c520f81d671490ac15e2723e94714fb6a5f5eca
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2021-07-15 20:44:38 -0700

    add ditlog tests

---
+++ demo/ditlog/ditlog.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+
+def init_w_axis(dx, log_wi):
+    log_w_min = np.min(log_wi)
+    log_w_max = np.max(log_wi) + 1e-4
+    N = np.ceil((log_w_max - log_w_min)/dx) + 1
+    log_w_arr = log_w_min + dx * np.arange(N)
+    return log_w_arr
+ 
+    
+def get_indices(arr_i, axis):
+    pos   = np.interp(arr_i, axis, np.arange(axis.size))
+    index = pos.astype(int) 
+    return index, index + 1, pos - index 
+
+
+def calc_matrix(v, log_wL, v0i, log_wLi, S0i, zero_pad=2):
+    
+    S_klm = np.zeros((zero_pad * v.size, log_wL.size))
+
+    ki0, ki1, avi = get_indices(v0i, v)          #Eqs 3.4 & 3.6
+    mi0, mi1, aLi = get_indices(log_wLi, log_wL) #Eqs 3.7 & 3.10
+
+    np.add.at(S_klm, (ki0, mi0), S0i * (1-avi) *   (1-aLi))
+    np.add.at(S_klm, (ki0, mi1), S0i * (1-avi) *      aLi )
+    np.add.at(S_klm, (ki1, mi0), S0i *    avi  *   (1-aLi))
+    np.add.at(S_klm, (ki1, mi1), S0i *    avi  *      aLi )
+    
+    return S_klm
+
+
+def calc_gV_FT(x, wL, folding_thresh):
+
+    gV_FT = lambda x,wL: np.exp(-np.pi*x*wL)
+    x_fold = (x, x[::-1])
+    
+    result = np.zeros(x.size)
+    n = 0
+    while gV_FT(n/2,wL) >= folding_thresh:
+        result += gV_FT(n/2 + x_fold[n&1], wL)
+        n += 1
+
+    return result
+
+
+def apply_transform(Nv, log_wL, S_klm, folding_thresh):
+    x      = np.fft.rfftfreq(S_klm.shape[0], 1)
+    S_k_FT = np.zeros(x.size, dtype = np.complex64)
+
+    for m in range(log_wL.size):
+        wL_m = np.exp(log_wL[m])
+        gV_FT = calc_gV_FT(x, wL_m, folding_thresh)    
+        S_k_FT += np.fft.rfft(S_klm[:,m]) * gV_FT
+    return np.fft.irfft(S_k_FT)[:Nv]
+
+
+# Call I = synthesize_spectrum(v, v0i, log_wGi, log_wLi, S0i) to synthesize the spectrum.
+# v = spectral axis (in cm-1)
+# v0i = list/array with spectral position of lines
+# log_wGi = list/array with log of the Gaussian widths of lines [= np.log(wGi)]
+# log_wLi = list/array with log of the Lorentzian widths of lines [= np.log(wLi)]
+# S0i = list/array with the linestrengths of lines (E.g. absorbances, emissivities, etc., depending the spectrum being synthesized)
+
+
+def synthesize_spectrum(v, v0i, log_wLi, S0i, dxv, dxL = 0.2,
+                        zero_pad = 2,
+                        folding_thresh = 1e-12):
+        
+    idx = (v0i >= np.min(v)) & (v0i < np.max(v))
+    v0i, log_wLi, S0i = v0i[idx], log_wLi[idx], S0i[idx]
+
+    log_dvi = np.log(v0i * dxv)
+    log_wL = init_w_axis(dxL, log_wLi - log_dvi) #pts
+    
+    S_klm = calc_matrix(v, log_wL, v0i, log_wLi - log_dvi, S0i, zero_pad)
+    dv = v*dxv
+    I = apply_transform(v.size, log_wL, S_klm, folding_thresh) / dv
+    return I
---
+++ demo/ditlog/lorentzian.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from ditlog import synthesize_spectrum as spectrum
+
+gLv  = lambda v,v0,w: 2/(np.pi*w) * 1 / (1 + 4*((v-v0)/w)**2)
+
+N = 200
+v0 = 100.0
+w = 1.0
+
+dxv = 1e-3
+dv = v0*dxv
+
+v_lin = v0 + np.arange(-N//2,N//2)*dv
+v_log = v0*np.exp(np.arange(-N//2,N//2)*dxv)
+
+plt.plot(v_lin, gLv(v_lin,v0,w),'-', label = 'linear grid')
+
+for zp in [2,4,8,16]:
+    I_ditlog = spectrum(v_log,
+                        np.array([v0]),
+                        np.array([np.log(w)]),
+                        np.array([1.0]),
+                        dxv,
+                        zero_pad = zp,
+                        folding_thresh = 1e-6
+                        )
+
+    plt.plot(v_log, I_ditlog, '--', label='ditlog ({:d}x)'.format(zp))
+
+plt.legend()
+plt.yscale('log')
+plt.show()
